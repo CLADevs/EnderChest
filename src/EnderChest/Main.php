36678dbed4199e49b69df7b709fedc847690bb10
iiFlamiinBlaze <iiflamiinblaze123@gmail.com>
2019-08-12 14:23:48 -0400

    Little cleanup

--- src/EnderChest/Main.php
+++ src/EnderChest/Main.php
@@ -7,46 +7,35 @@ namespace EnderChest;
 use pocketmine\block\Block;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
-use pocketmine\level\Position;
+use pocketmine\nbt\tag\CompoundTag;
+use pocketmine\nbt\tag\IntTag;
+use pocketmine\nbt\tag\StringTag;
 use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
 use pocketmine\tile\EnderChest;
 use pocketmine\tile\Tile;
+use pocketmine\utils\TextFormat;
 
 class Main extends PluginBase{
 
-	private static $instance;
-
-	public function onEnable(): void{
-		self::$instance = $this;
+	public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
+		if(strtolower($command->getName()) === "enderchest"){
+			if(!$sender instanceof Player){
+				$sender->sendMessage(TextFormat::RED . "Use this command in-game");
+				return false;
+			}
+			$nbt = new CompoundTag("", [new StringTag("id", Tile::CHEST), new StringTag("CustomName", "EnderChest"), new IntTag("x", (int)floor($sender->x)), new IntTag("y", (int)floor($sender->y) - 4), new IntTag("z", (int)floor($sender->z))]);
+			/** @var EnderChest $tile */
+			$tile = Tile::createTile("EnderChest", $sender->getLevel(), $nbt);
+			$block = Block::get(Block::ENDER_CHEST);
+			$block->x = (int)$tile->x;
+			$block->y = (int)$tile->y;
+			$block->z = (int)$tile->z;
+			$block->level = $tile->getLevel();
+			$block->level->sendBlocks([$sender], [$block]);
+			$sender->getEnderChestInventory()->setHolderPosition($tile);
+			$sender->addWindow($sender->getEnderChestInventory());
+		}
+		return true;
 	}
-
-	public static function get(): self{
-		return self::$instance;
-	}
-
-	public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool{
-	    if($command->getName() === "enderchest"){
-	        if($sender instanceof Player){
-                $chest = null;
-	            $cache = null;
-	            $pos = new Position($sender->getX(), $sender->getY() + 3, $sender->getZ(), $sender->getLevel());
-	            $cache = $sender->getLevel()->getBlock($pos);
-	            $block = Block::get(Block::ENDER_CHEST);
-	            $block->setComponents(intval($pos->getX()), intval($pos->getY()), intval($pos->getZ()));
-                $sender->getLevel()->sendBlocks([$sender], [$block]);
-                $tile = $sender->getLevel()->getTile($pos);
-                if($tile instanceof EnderChest){
-                    $chest = $tile;
-                }else{
-                    $chest = Tile::createTile(Tile::ENDER_CHEST, $sender->getLevel(), EnderChest::createNBT($pos));
-                }
-                $inv = new EnderChestInventory();
-                $inv->setHolderPosition($chest);
-                $inv->cache = $cache;
-                $sender->addWindow($inv);
-            }
-        }
-	    return true;
-    }
 }
